7f5bbb88ed9899c2965e36da7b42d5af26657b4b
Mary Franks <maryfranks@gmail.com>
2018-01-05 12:41:37 -0500

    show all comments on post

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ import Comment from './Comment.js';
 class Post extends Component {
   // what should the component render?
   render () {
+    let allComments = [
+      <Comment body={this.props.comments[0]} />,
+      <Comment body={this.props.comments[1]} />,
+      <Comment body={this.props.comments[2]} />
+    ]
     // make sure to return some UI
     return (
       <div>
-      <h1>{this.props.title}</h1>
-      <h3>by {this.props.author}</h3>
-      <p>{this.props.body}</p>
-      <Comment body={this.props.comments[0]} />
+        <h1>{this.props.title}</h1>
+        <h2>by {this.props.author}</h2>
+        <div>
+          <p>{this.props.body}</p>
+        </div>
+        <h3>Comments:</h3>
+        {allComments}
       </div>
     )
   }
