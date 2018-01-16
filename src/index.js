90b6ef959f92740436ded4cbe4d71413e8a2ed64
Mary Franks <maryfranks@gmail.com>
2018-01-16 11:39:47 -0500

    remove duplicate code that I moved to App.js earlier

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Post from './Post';
+import App from './App';
 import './index.css'
 
-var post = {
-  title: "Dinosaurs are awesome",
-  authors: [
-    "Stealthy Stegoasaurus",
-    "Tiny T-Rex",
-    "Ivory Iguanodon"
-  ],
-  body: "Check out this body property!",
-  comments: [
-    "First!",
-    "Great post",
-    "Hire this author now!"
-  ]
-}
 
 ReactDOM.render(
-  <Post
-  title={post.title}
-  authors={post.authors}
-  body={post.body}
-  comments={post.comments}/>,
+  <App />,
   document.getElementById('root')
 );
